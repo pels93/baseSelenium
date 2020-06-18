b68884b73151c1aef351641c3f272ae4b6aea043
danigonz <daniel-g-j@hotmail.com>
2020-06-18 12:00:23 +0200

    update .bat

--- set_variables_javaMaven_2.bat
+++ set_variables_javaMaven_2.bat
@@ -1,2 +1,10 @@
 @echo off
 SETX /M PATH "%PATH%;%M2_HOME%\bin;%JAVA_HOME%\bin"
+echo ---------------------------------------------
+echo ejecuta los siguientes comandos en un nuevo terminal
+echo ---------------------------------------------
+echo java -version
+echo mvn --version
+echo.
+echo.
+pause
