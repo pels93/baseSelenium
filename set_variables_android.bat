e19a2fa30a2f884fc99ab0a4ed1266b0949fb239
danigonz <daniel-g-j@hotmail.com>
2020-06-18 11:54:03 +0200

    update .bat

--- set_variables_android.bat
+++ set_variables_android.bat
@@ -2,4 +2,3 @@
 SETX /M ANDROID_HOME "%LOCALAPPDATA%\Android\Sdk"
 SETX /M ANDROID_SDK_HOME "%SystemDrive%%HOMEPATH%\.android"
 SETX /M ANDROID_AVD_HOME "%SystemDrive%%HOMEPATH%\.android\avd"
-start "MyWindow" cmd /c "ping localhost & ipconfig & pause"
