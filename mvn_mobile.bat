2b3a1ad0e8114b94f376308faba6d589af2b760d
danigonz <daniel-g-j@hotmail.com>
2020-07-09 12:29:53 +0200

    update variable tags

--- mvn_mobile.bat
+++ mvn_mobile.bat
@@ -1,9 +1,11 @@
 @echo off
 adb kill-server
 adb kill-server
+SET url=http://127.0.0.1:4723/wd/hub
+SET tags=@mobile
 start mvn clean
 cls
-(echo cucumber.typeDriver=appium&echo urlServerAppium=http://127.0.0.1:4723/wd/hub&echo cucumber.tags=--tags "@mobile"&echo cucumber.mobilePlatform=Android&echo cucumber.mobileLanguage=ES_es&echo cucumber.app=test.apk&echo cucumber.nameMobile=Pixel_2_API_27&echo cucumber.versionMobile=8.1.0&echo cucumber.adbName=emulator-5556&echo #cucumber set&echo cucumber.TypeTest=${cucumber.typeDriver}_${cucumber.app}_${cucumber.nameMobile}&echo cucumber.report=--plugin json:reports/json/${cucumber.TypeTest}/report.json --plugin pretty --plugin summary&echo cumcumber.reportHTML=reports/html/${cucumber.TypeTest}/&echo cumcumber.reportJSON=reports/json/${cucumber.TypeTest}/) > src\test\java\RunCucumber.properties
+(echo cucumber.typeDriver=appium&echo urlServerAppium=%url%&echo cucumber.tags=--tags "%tags%"&echo cucumber.mobilePlatform=Android&echo cucumber.mobileLanguage=ES_es&echo cucumber.app=test.apk&echo cucumber.nameMobile=Pixel_2_API_27&echo cucumber.versionMobile=8.1.0&echo cucumber.adbName=emulator-5556&echo #cucumber set&echo cucumber.TypeTest=${cucumber.typeDriver}_${cucumber.app}_${cucumber.nameMobile}&echo cucumber.report=--plugin json:reports/json/${cucumber.TypeTest}/report.json --plugin pretty --plugin summary&echo cumcumber.reportHTML=reports/html/${cucumber.TypeTest}/&echo cumcumber.reportJSON=reports/json/${cucumber.TypeTest}/) > src\test\java\RunCucumber.properties
 start %ANDROID_HOME%\tools\emulator.exe -avd Pixel_2_API_27 -no-snapshot -no-snapshot-load -no-snapshot-save -memory 512 -timezone Europe/Paris -no-boot-anim -no-audio -nocache
 start %ANDROID_HOME%\tools\emulator.exe -avd Pixel_2_API_27_2 -no-snapshot -no-snapshot-load -no-snapshot-save -memory 512 -timezone Europe/Paris -no-boot-anim -no-audio -nocache
 ping -n 30 127.0.0.1 >nul
@@ -11,5 +13,5 @@ adb devices
 cls
 start mvn post-integration-test -Dmaven.test.failure.ignore=true
 ping -n 20 127.0.0.1 >nul
-(echo cucumber.typeDriver=appium&echo urlServerAppium=http://127.0.0.1:4723/wd/hub&echo cucumber.tags=--tags "@mobile"&echo cucumber.mobilePlatform=Android&echo cucumber.mobileLanguage=ES_es&echo cucumber.app=test.apk&echo cucumber.nameMobile=Pixel_2_API_27_2&echo cucumber.versionMobile=8.1.0&echo cucumber.adbName=emulator-5554&echo #cucumber set&echo cucumber.TypeTest=${cucumber.typeDriver}_${cucumber.app}_${cucumber.nameMobile}&echo cucumber.report=--plugin json:reports/json/${cucumber.TypeTest}/report.json --plugin pretty --plugin summary&echo cumcumber.reportHTML=reports/html/${cucumber.TypeTest}/&echo cumcumber.reportJSON=reports/json/${cucumber.TypeTest}/) > src\test\java\RunCucumber.properties
+(echo cucumber.typeDriver=appium&echo urlServerAppium=%url%&echo cucumber.tags=--tags "%tags%"&echo cucumber.mobilePlatform=Android&echo cucumber.mobileLanguage=ES_es&echo cucumber.app=test.apk&echo cucumber.nameMobile=Pixel_2_API_27_2&echo cucumber.versionMobile=8.1.0&echo cucumber.adbName=emulator-5554&echo #cucumber set&echo cucumber.TypeTest=${cucumber.typeDriver}_${cucumber.app}_${cucumber.nameMobile}&echo cucumber.report=--plugin json:reports/json/${cucumber.TypeTest}/report.json --plugin pretty --plugin summary&echo cumcumber.reportHTML=reports/html/${cucumber.TypeTest}/&echo cumcumber.reportJSON=reports/json/${cucumber.TypeTest}/) > src\test\java\RunCucumber.properties
 start mvn post-integration-test -Dmaven.test.failure.ignore=true
